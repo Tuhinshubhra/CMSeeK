2f958d2c3896185e891ddb1a54f59626d10b393f
Tuhinshubhra <r3dhax0rofficial@gmail.com>
2019-05-18 20:44:32 +0530

    Fixed an issue where cms name and cms url would not be added in the result json

--- cmseekdb/core.py
+++ cmseekdb/core.py
@@ -157,6 +157,8 @@ def main_proc(site,cua):
             cmseek.banner("CMS Scan Results")
             result.target(site)
             result.cms(cms_info['name'],'0',cms_info['url'])
+            cmseek.update_log('cms_name', cms_info['name']) # update log
+            cmseek.update_log('cms_url', cms_info['url']) # update log
             comptime = round(time.time() - cmseek.cstart, 2)
             log_dir = cmseek.log_dir
             if log_dir is not "":
